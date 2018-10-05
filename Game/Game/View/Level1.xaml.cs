648ae0b9ef8cdbaf0266703517f0d297d1495dff
Jeffrey Kolkman <jeffrey.online@hotmail.com>
2018-10-05 11:11:11 +0200

    level update

--- Game/Game/View/Level1.xaml.cs
+++ Game/Game/View/Level1.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Game.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Game
 {
@@ -19,9 +21,24 @@ namespace Game
     /// </summary>
     public partial class Level1 : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+
+        public Player Player { get; set; }
+         
         public Level1()
         {
             InitializeComponent();
+
+            Player = new Player(new Point(0,0));
+
+            timer.Tick += TimerOnTick;
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            timer.Start();
+        }
+
+        private void TimerOnTick(object sender, EventArgs e)
+        {
+            Player.Move(Player.Locatie, Player);
         }
     }
 }
