122ebf18e7e4e27b81d1abe285375bf1df914a09
Xuwei <xuwei.hu@hotmail.com>
2018-10-15 10:44:08 +0200

    Candy class aangemaakt

---
+++ Game/Game/Model/Candy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Game.Model
+{
+    class Candy : Prop
+    {
+        public int Score = 100;
+
+        public Candy(Point position)
+        {
+            Position = position;
+            Size = new Point(36, 36);
+            Image = "candy.png";
+        }
+        
+    }
+}
