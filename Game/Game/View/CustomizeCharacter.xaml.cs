dc9debb52d3ae2b443cdc1e5418faa09ad00120a
MikeSchrijver <m.schrijver@hotmail.com>
2018-10-16 10:34:37 +0200

    Customize Character done + Restart in Pause screen

--- Game/Game/View/CustomizeCharacter.xaml.cs
+++ Game/Game/View/CustomizeCharacter.xaml.cs
@@ -21,18 +21,56 @@ namespace Game
     public partial class CustomizeCharacter : Window
     {
         public static string character = "";
+        private int player = 1;
 
         public CustomizeCharacter()
         {
            InitializeComponent();
+
+           Ad.Opacity = 1;
+           El.Opacity = 0.5;
+           Pi.Opacity = 0.5;
         }
 
         private void character_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("You chose " + ((Button)sender).Name.ToString());
-            Button targetButton = (sender as Button);
-            
-            character = targetButton.Name;
+        Button targetButton = (sender as Button);
+        character = targetButton.Name;
+
+            if(targetButton.Name == "Adventure")
+            {
+                player = 1;
+            } else if (targetButton.Name == "Elf")
+            {
+                player = 2;
+            } else if (targetButton.Name == "Pirate")
+            {
+                player = 3;
+            } else
+            {
+                player = 1;
+            }
+
+            if (player == 1)
+            {
+                Ad.Opacity = 1;
+                El.Opacity = 0.5;
+                Pi.Opacity = 0.5;
+            }
+            else if (player == 2)
+            {
+                Ad.Opacity = 0.5;
+                El.Opacity = 1;
+                Pi.Opacity = 0.5;
+            }
+            else if (player == 3)
+            {
+                Ad.Opacity = 0.5;
+                El.Opacity = 0.5;
+                Pi.Opacity = 1;
+            }
+
+
 
         }
 
