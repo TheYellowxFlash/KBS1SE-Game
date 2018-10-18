9b027e43af5aa6017ef42909e70d008eb9c34744
Unknown <rutgerhol@hotmail.com>
2018-10-18 09:36:56 +0200

    Commentaar en encapsulation

--- Game/Game/Model/Walker.cs
+++ Game/Game/Model/Walker.cs
@@ -11,29 +11,35 @@ namespace Game.Model
 {
     class Walker : Prop
     {
-        public enum HorizontalDirection { left,right, none};
-        public enum VerticalDirection { up, down, none}
-        public bool canMoveThroughWalls = false;
-        public double movementSpeed;
+        protected enum HorizontalDirection { left,right, none};
+        protected enum VerticalDirection { up, down, none}
+        protected bool canMoveThroughWalls = false;
+        protected double movementSpeed;
 
-        public Walker(Point positionPoint) : base(positionPoint)
+        protected Walker(Point positionPoint) : base(positionPoint)
         {
 
         }
 
-        public Walker(Point position, Point size, String image) : base(position, size, image)
+        protected Walker(Point position, Point size, string image) : base(position, size, image)
         {
             Position = position;
             Size = size;
             Image = image;
         }
 
-        public Walker(Point position, String image) : base(position, image)
+        protected Walker(Point position, string image) : base(position, image)
         {
             Position = position;
             Image = image;
         }
 
+        /// <summary>
+        /// Checks if a walker can and wants to move and then moves it
+        /// </summary>
+        /// <param name="horizontal">The Horizontal direction(Left, Right or none)</param>
+        /// <param name="vertical">The Vertical direction(Up, Down or none)</param>
+        /// <param name="obstacles">A list of all the obstacles the walker can't move through</param>
         protected void Move(HorizontalDirection horizontal, VerticalDirection vertical, List<Obstacle> obstacles)
         {
             //if both directions are none the walker doesn't want to move
@@ -42,19 +48,22 @@ namespace Game.Model
             double y = Position.Y;
             double x = Position.X;
             
+            //get if the walker wants to move and check if the walker is at one of the edges if so don't allow it to move through the edge
             bool moveRight = horizontal == HorizontalDirection.right && x + Size.X + movementSpeed < World.windowWidth;
             bool moveLeft = horizontal == HorizontalDirection.left && x - movementSpeed >= 0;
             bool moveDown = vertical == VerticalDirection.down && y + Size.Y + movementSpeed < World.windowHeight;
             bool moveUp = vertical == VerticalDirection.up && y - movementSpeed >= 0;
+            //some Walkers like Enemy.Ghost can move through walls so skip all obstacle collision detection for these Walkers
             if (!canMoveThroughWalls)
             {
-                foreach (var obstacle in obstacles)
+                foreach (Obstacle obstacle in obstacles)
                 {
                     //if all directions are false stop the loop
                     if (!(moveUp || moveDown || moveLeft || moveRight))
                         break;
-
-                    if (moveRight && ((Position.X + Size.X + movementSpeed > obstacle.Position.X) && (Position.X + Size.X + movementSpeed < obstacle.Position.X + obstacle.Size.X) &&
+                    //check if the walker is not colliding with an object to its right
+                    if (moveRight && 
+                        ((Position.X + Size.X + movementSpeed > obstacle.Position.X) && (Position.X + Size.X + movementSpeed < obstacle.Position.X + obstacle.Size.X) &&
                         (Position.Y < obstacle.Position.Y + obstacle.Size.Y) && (obstacle.Position.Y < Position.Y + Size.Y)))
                     {
                         moveRight = false;
